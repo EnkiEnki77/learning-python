8ed73b9f2f3bf61e26956f9deede3d882234c9d6
Enki Layman <layman212@gmail.com>
2024-06-16 02:21:54 -0400

    multiple variable declarations

---
+++ variables/multi_variable_declarations.py
@@ -0,0 +1,11 @@
+#we save space by declaring multiple variables on the same line
+
+sword_name, sword_damage, sword_length = "Excalibur", 10, 200
+
+print(sword_name)
+print(sword_damage)
+print(sword_length)
+
+#Any number of variables can be declared on the same line, but the convention is they should be variables that are related
+#to eachother to keep things readable and easy to understand. This is a clean code best practice.
+
