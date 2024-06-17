b4579c0f43789b287cbad9bde837e437854a8618
Enki Layman <layman212@gmail.com>
2024-06-17 01:27:47 -0400

    binary numbers

---
+++ Computing/binary_numbers.py
@@ -0,0 +1,11 @@
+# binary numbers are base 2 numbers, they have the option of either 0 or 1. Each digit of a binary number is equivalent
+# to 2 times the value of the previous digit starting from 1 right to left, but that value is only added to the total if that digit is
+# represented as 1, if its 0 it isnt 
+
+#       1 is 1
+#      10 is 2
+#     100 is 4
+#    1000 is 8
+#   10000 is 16
+#  100000 is 32
+# 1000000 is 64 
